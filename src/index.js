47f14dc204474f96068313168b99b5027bd52013
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-06-16 23:53:47 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
+  function taskToDo(todo) {
+    let li = document.createElement("li")
+    li.textContent = `${todo}`
+    let liBtn = document.createElement("button")
+    liBtn.addEventListener("click",  handleDelete)
+    liBtn.textContent = "x"
+    li.appendChild(liBtn)
+    document.querySelector("#tasks").appendChild(li)
+  }
+let form = document.querySelector("#create-task-form")
+ form.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+  taskToDo(document.getElementById("new-task-description").value)
+  form.reset()
+  })
 });
